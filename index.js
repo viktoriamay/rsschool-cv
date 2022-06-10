dbe7559b9c75a0271172baf947463d59df794fa4
viktoriamay <alterkelly@mail.ru>
2022-06-10 16:36:57 +0300

    feat: add burger menu

---
+++ index.js
@@ -0,0 +1,9 @@
+const iconMenu = document.querySelector('.header__menu-icon');
+const menuBody = document.querySelector('.nav');
+if (iconMenu) {
+    iconMenu.addEventListener('click', function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
